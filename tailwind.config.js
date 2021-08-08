a867388db30a848893ce9eff1ea3552b85d9a5a7
Ramsay Romero <Ramsay.Romero@gmail.com>
2021-08-08 13:43:53 -0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
-    enabled: true,
     content: [
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
